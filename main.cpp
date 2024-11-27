6c1b65d13523ee979f8bebb503866bd95b8c1c73
miguelmsantoss <miguelm@defensepoint.com>
2024-11-27 09:12:54 -0300

    Changing to buffer overflow

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include <cstring>
+
+void bufferOverflow() {
+    char buffer[10];
+    const char* largeInput = "This text is too long for the buffer!";
+
+    std::strcpy(buffer, largeInput);
+
+    std::cout << "Buffer: " << buffer << std::endl;
+}
 
 int main() {
     std::cout << "Hello, Coverity!" << std::endl;
 
-    char* ptr = nullptr;
-    if (ptr) {
-        *ptr = 'A';
-    }
+    bufferOverflow();
 
     return 0;
 }
